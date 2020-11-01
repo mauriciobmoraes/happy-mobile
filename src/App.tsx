42e35eaec2ed59bc19634ce8c6c6284e51da6a74
unknown <mauriciobmoraes2012@gmail.com>
2020-10-31 21:13:31 -0300

    iniciando projeto happy-mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="HAPPY"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          1
         </a>
-      </header>
+      </div>
     </div>
   );
 }
